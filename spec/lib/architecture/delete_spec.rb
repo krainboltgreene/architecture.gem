d14148beccec7b14a03d74e4c1793ec3f889a589
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-01-02 12:54:43 -0600

    Cleaning up and adding tests.

---
+++ spec/lib/architecture/delete_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+RSpec.describe Architecture::Delete do
+  let("entity") do
+    instance_double(Architecture::Entity)
+  end
+
+  let("source") do
+    "old"
+  end
+
+  let("delete") do
+    described_class.new(source: source)
+  end
+
+  describe "#call" do
+    let("call") do
+      delete.call
+    end
+
+    before("each") do
+      allow(delete).to receive("entity").and_return(entity)
+      allow(entity).to receive("location").and_return(source)
+    end
+
+    after("each") do
+      call
+    end
+
+    it "moves the file" do
+      expect(entity).to receive("delete")
+    end
+  end
+end
