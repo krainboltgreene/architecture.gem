ff59dc2a5b699af0136d69aec5eefc93f2f49ff6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-01-19 05:33:45 -0600

    Finally testing entity, fixing entity location

--- lib/architecture/entity.rb
+++ lib/architecture/entity.rb
@@ -1,55 +1,62 @@
 module Architecture
   class Entity
+    DIRECTORY_MIMETYPE = "application/x-directory"
+    DEFAULT_ENGINE = ::File
+
     def initialize(path:)
       @path = path
     end
 
-    def location
-      ::File.expand_path(location)
+    def location(engine: DEFAULT_ENGINE)
+      if engine.respond_to?("expand_path")
+        engine.expand_path(path)
+      else
+        path
+      end
     end
 
-    def content
-      if file?
-        ::File.read(location)
+    def content(engine: DEFAULT_ENGINE)
+      if file?(engine: engine)
+        engine.read(location(engine: engine))
       else
         ""
       end
     end
 
-    def write(text:)
-      ::File.write(location, text)
+    def write(text:, engine: DEFAULT_ENGINE)
+      engine.write(location(engine: engine), text)
     end
 
-    def copy(entity:)
-      system("cp #{location} #{entity.location}")
+    def copy(entity:, engine: DEFAULT_ENGINE)
+      system("cp #{location(engine: engine)} #{entity.location(engine: engine)}")
     end
 
-    def move(entity:)
-      system("mv #{location} #{entity.location}")
+    def move(entity:, engine: DEFAULT_ENGINE)
+      system("mv #{location(engine: engine)} #{entity.location(engine: engine)}")
     end
 
-    def delete
-      system("rm -rf #{location}")
+    def delete(engine: DEFAULT_ENGINE)
+      system("rm -rf #{location(engine: engine)}")
     end
 
-    def create
-      system("mkdir -p #{location}")
+    def create(engine: DEFAULT_ENGINE)
+      system("mkdir -p #{location(engine: engine)}")
+    end
+
+    def file?(engine: DEFAULT_ENGINE)
+      !directory?(engine: engine)
+    end
+
+    def directory?(engine: DEFAULT_ENGINE)
+      type(engine: engine) == DIRECTORY_MIMETYPE
     end
 
     private def path
       @path
     end
 
-    def file?
-      !directory?
-    end
-
-    def directory?
-      type == "application/x-directory"
-    end
-
-    private def type
-      system("file --mime-type --brief #{location}")
+    private def type(engine: DEFAULT_ENGINE)
+      system("file --mime-type --brief #{location(engine: engine)}")
     end
 
     private def system(command)
---
+++ spec/lib/architecture/entity_spec.rb
@@ -0,0 +1,160 @@
+require "spec_helper"
+
+RSpec.describe Architecture::Entity do
+  let("path") do
+    "foo/bar/baz"
+  end
+
+  let("entity") do
+    described_class.new(path: path)
+  end
+
+  let("engine") do
+    double(File)
+  end
+
+  before("each") do
+    allow(entity).to receive("system")
+  end
+
+  describe "#directory?" do
+    let("directory?") do
+      entity.directory?
+    end
+
+    context "when path points to a directory" do
+      it "returns true" do
+        allow(entity).to receive("type").and_return("application/x-directory")
+        expect(directory?).to be(true)
+      end
+    end
+
+    context "when path points to a file" do
+      it "returns false" do
+        allow(entity).to receive("type").and_return("application/x-file")
+        expect(directory?).to be(false)
+      end
+    end
+  end
+
+  describe "#file?" do
+    let("file?") do
+      entity.file?
+    end
+
+    context "when path points to a file" do
+      it "returns true" do
+        allow(entity).to receive("type").and_return("application/x-file")
+        expect(file?).to be(true)
+      end
+    end
+
+    context "when path points to a directory" do
+      it "returns false" do
+        allow(entity).to receive("type").and_return("application/x-directory")
+        expect(file?).to be(false)
+      end
+    end
+  end
+
+  describe "#create" do
+    let("create") do
+      entity.create(engine: engine)
+    end
+
+    it "calls the system" do
+      allow(engine).to receive("expand_path").and_return(path)
+      expect(entity).to receive("system")
+      create
+    end
+  end
+
+  describe "#delete" do
+    let("delete") do
+      entity.delete
+    end
+
+    it "calls the system" do
+      allow(engine).to receive("expand_path").and_return(path)
+      expect(entity).to receive("system")
+      delete
+    end
+  end
+
+  describe "#move" do
+    let("move") do
+      entity.move(entity: entity)
+    end
+
+    it "calls the system" do
+      allow(engine).to receive("expand_path").and_return(path)
+      expect(entity).to receive("system")
+      move
+    end
+  end
+
+  describe "#copy" do
+    let("copy") do
+      entity.copy(entity: entity)
+    end
+
+    it "calls the system" do
+      allow(engine).to receive("expand_path").and_return(path)
+      expect(entity).to receive("system")
+      copy
+    end
+  end
+
+  describe "#write" do
+    let("write") do
+      entity.write(text: "foo", engine: engine)
+    end
+
+    it "writes to the engine" do
+      allow(engine).to receive("expand_path").and_return(path)
+      expect(engine).to receive("write")
+      write
+    end
+  end
+
+  describe "#content" do
+    let("content") do
+      entity.content(engine: engine)
+    end
+
+    context "with a path that points to a file" do
+      it "reads from the engine" do
+        allow(entity).to receive("type").and_return("application/x-file")
+        allow(engine).to receive("expand_path").and_return(path)
+        expect(engine).to receive("read")
+        content
+      end
+    end
+
+    context "with a path that points to a directory" do
+      it "returns a empty string" do
+        allow(entity).to receive("type").and_return("application/x-directory")
+        expect(content).to eq("")
+      end
+    end
+  end
+
+  describe "#location" do
+    let("location") do
+      entity.location(engine: engine)
+    end
+
+    context "if engine responds to expand_path" do
+      it "calls expand_path on the engine" do
+        expect(engine).to receive("expand_path")
+        location
+      end
+    end
+
+    context "if engine doesn't respond to expand_path" do
+      it "returns the path" do
+        expect(location).to eq(path)
+      end
+    end
+  end
+end
