8656d59838ad39cadffa52e8ee063caee0189169
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-07-08 07:30:09 -0500

    Rewriting to use the new private API

--- lib/architecture/dsl.rb
+++ lib/architecture/dsl.rb
@@ -11,48 +11,109 @@ module Architecture
       yield(self)
     end
 
-    def copy(file: nil, directory: nil, as: nil, context: {})
-      ::Architecture::Copy.new(source: source(directory || file), destination: destination(as || directory || file), context: context).call
+    def copy(file: nil, directory: nil, as: nil, context: EMPTY_CONTEXT, &block)
+      a = Entity.new(id: directory || file, prefix: @source)
+      b = Entity.new(id: as || directory || file, prefix: @destination)
+
+      @output.print("#{indentention}Copying #{a} to #{b}")
+
+      Copy.new(source: a, destination: b, context: context)
+
+      @output.puts(" succeeded.")
+
+      if block_given? && directory
+        within(directory: directory)
+      end
     end
 
-    def move(file: nil, directory: nil, as:)
-      ::Architecture::Move.new(source: directory || file, destination: as).call
+    def move(file: nil, directory: nil, as:, &block)
+      a = Entity.new(id: directory || file, prefix: @source)
+      b = Entity.new(id: as, prefix: @destination)
+
+      @output.print("#{indentention}Moving #{a} to #{b}")
+
+      Move.new(source: a, destination: b)
+
+      @output.puts(" succeeded.")
+
+      if block_given? && directory
+        within(directory: directory)
+      end
     end
 
-    def create(directory: nil, file: nil, content: nil, context: {})
-      ::Architecture::Create.new(source: directory || file, content: content, context: context).call
+    def create(file: nil, directory: nil, content: nil, context: EMPTY_CONTEXT, location: nil, &block)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Creating #{a}")
+
+      Create.new(source: a, content: content, context: context)
+
+      @output.puts(" succeeded.")
+
+      if block_given? && directory
+        within(directory: directory)
+      end
     end
 
-    def delete(directory: nil, file: nil)
-      ::Architecture::Delete.new(source: directory || file).call
+    def delete(directory: nil, file: nil, location: nil)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Deleting #{a}")
+
+      Delete.new(source: a)
+
+      @output.puts(" succeeded.")
     end
 
-    def replace(file: nil, search:, content:)
-      ::Architecture::Replace.new(source: file, search: search, content: content).call
+    def replace(file:, search:, content:, location: nil)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Replacing content in #{a}")
+
+      Replace.new(source: a, search: search, content: content)
+
+      @output.puts(" succeeded.")
     end
 
-    def prepend(file: nil, content:, context: {})
-      ::Architecture::Prepend.new(source: file, content: content, context: context).call
+    def prepend(file:, content:, context: Architecture::EMPTY_CONTEXT, location: nil)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Prepending #{a} with content")
+
+
+      Prepend.new(source: a, content: content, context: context)
+
+      @output.puts(" succeeded.")
     end
 
-    def append(file: nil, content:, context: {})
-      ::Architecture::Append.new(source: file, content: content, context: context).call
+    def append(file:, content:, context: Architecture::EMPTY_CONTEXT, location: nil)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Appending #{a}")
+
+      Append.new(source: a, content: content, context: context)
+
+      @output.puts(" succeeded.")
     end
 
-    def overwrite(file: nil, content:, context: {})
-      ::Architecture::Overwrite.new(source: file, content: content, context: context).call
+    def overwrite(file:, content:, context: Architecture::EMPTY_CONTEXT, location: nil)
+      a = Entity.new(id: directory || file, prefix: location || @destination)
+
+      @output.print("#{indentention}Appending #{a}")
+
+      Overwrite.new(source: a, content: content, context: context)
+
+      @output.puts(" succeeded.")
     end
 
-    def within(source: nil, destination: nil, &block)
-      ::Architecture::DSL.new(source: source(source), destination: destination(destination), &block)
+    def within(directory: nil, source: join(@source, directory), destination: join(@destination, directory), &block)
+      @output.print "#{indentention}Within #{destination}"
+
+      self.class.new(source: source, destination: destination, output: @output, level: @level + 1, &block)
     end
 
-    def source(path)
-      join(@source, path)
-    end
-
-    def destination(path)
-      join(@destination, path)
+    private def join(*ids)
+      File.join(*ids)
     end
 
     private def indentention
