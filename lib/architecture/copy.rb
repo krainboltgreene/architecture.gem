1b01d10d5eb8733d9f3611ef78281207805f80e4
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-07-08 07:28:51 -0500

    Allowing for more composability by DI the entities

--- lib/architecture/copy.rb
+++ lib/architecture/copy.rb
@@ -3,9 +3,9 @@ module Architecture
     include Replicatable
     include Contentable
 
-      @origin = Entity.new(path: source)
-      @clone = Entity.new(path: destination)
     def initialize(source:, destination:, context: Architecture::EMPTY_CONTEXT)
+      @origin = source
+      @clone = destination
       @context = context
     end
 
